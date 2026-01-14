611633fc7ade74b6be2f2d77e6f62789b9d03177
Ananya jantli <jantliananya@gmail.com>
2026-01-14 22:38:50 +0530

    Update employee_salary.py

--- employee_salary.py
+++ employee_salary.py
@@ -1,43 +1,17 @@
 import sys
 
-def calculate_salary(name, emp_id, dept, basic_salary):
-    print("==== Employee Salary Processing ====")
+print("==== Employee Salary Processing ====")
 
-    hra = basic_salary * 0.20
-    da = basic_salary * 0.10
-    gross_salary = basic_salary + hra + da
+name = sys.argv[1]
+emp_id = sys.argv[2]
+dept = sys.argv[3]
+s1 = int(sys.argv[4])
+s2 = int(sys.argv[5])
+s3 = int(sys.argv[6])
 
-    if basic_salary <= 50000:
-        tax = 0
-    elif basic_salary <= 80000:
-        tax = gross_salary * 0.10
-    else:
-        tax = gross_salary * 0.20
+avg = (s1 + s2 + s3) / 3
 
-    net_salary = gross_salary - tax
-
-    print("\n==== Result ====")
-    print(f"Employee Name : {name}")
-    print(f"Employee ID   : {emp_id}")
-    print(f"Department    : {dept}")
-    print(f"Basic Salary  : {basic_salary}")
-    print(f"Gross Salary  : {gross_salary}")
-    print(f"Net Salary    : {net_salary}")
-
-
-if __name__ == "__main__":
-    # Jenkins / Docker (non-interactive)
-    if len(sys.argv) == 5:
-        name = sys.argv[1]
-        emp_id = sys.argv[2]
-        dept = sys.argv[3]
-        salary = float(sys.argv[4])
-    else:
-        # Local / interactive
-        print("==== Employee Salary Processing ====")
-        name = input("Enter employee name: ")
-        emp_id = input("Enter employee ID: ")
-        dept = input("Enter department: ")
-        salary = float(input("Enter basic salary: "))
-
-    calculate_salary(name, emp_id, dept, salary)
+print("Employee Name :", name)
+print("Employee ID   :", emp_id)
+print("Department    :", dept)
+print("Average Score :", avg)
